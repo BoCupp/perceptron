6f6aa5aceb9ed1f02770d22f3043ecf906d9483f
Bo Cupp <bo@seattlecupps.com>
2016-09-22 04:16:43 -0700

    fixing bugs, updating tests

--- perceptron.js
+++ perceptron.js
@@ -1,13 +1,21 @@
-function Perceptron(weights) {
-	if (typeof weights === "undefined") {
-		this.weights = [];
+function Perceptron(weights, classifierFunction) {
+	if (weights instanceof Array) {
+		this.weights = weights;
 	} else {
-		this.weights = weights;		
+		this.weights = [];
+	}
+
+	if (classifierFunction instanceof Function) {
+		this.classifierFunction = classifierFunction;
+	} else {
+		this.classifierFunction = function(weightedSum) { 
+			return weightedSum >= 0 ? +1 : -1;
+		}
 	}
 }
 
 Perceptron.prototype = {
-	react: function Perceptron_generate(stimulus) {
+	react: function Perceptron_react(stimulus) {
 		var weightedSum = 0;
 		var i;
 
@@ -15,24 +23,30 @@ Perceptron.prototype = {
 			weightedSum = weightedSum + stimulus[i] * this.weights[i];
 		}
 
-		return weightedSum;
+		weightedSum += /* hard-coded input of 1 allows last weight to be learned bias */ 1 * this.weights[stimulus.length];
+
+
+		return this.classifierFunction(weightedSum);
 	},
 
 	learn: function Perceptron_learn(stimulus, desired) {
-		while (stimulus.length > this.weights.length) {
+		// 1 + stimulus vector size allows room for last weight to represent learned bias, where input will be hard coded to 1
+		while (stimulus.length + 1 > this.weights.length) {
 			this.weights.push(0);
 		}
 
 		var actual = this.react(stimulus);
-		var delta = desired - actual;
-
-		var learningRate = 0.2;
 		
-		for (var i = 0; i < stimulus.length; i++) {
-			if (stimulus[i] != 0) {
-				this.weights[i] = this.weights[i] + delta / stimulus[i] * learningRate;
-			}
+		if (actual == desired) {
+			return;
 		}
+
+		// Adjust for error
+		for (var i = 0; i < stimulus.length; i++) {
+			this.weights[i] = this.weights[i] + desired * stimulus[i];
+		}
+		// And learned bias adjustment
+		this.weights[stimulus.length] = this.weights[stimulus.length] + desired;
 	}
 };
 
